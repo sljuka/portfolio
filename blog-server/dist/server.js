345acfbd60c524f4cce226ba436bf7db947912bd
David Sljukic <david.sljukic.dev@gmail.com>
2019-10-03 12:26:00 +0200

    Add initial gql schema

--- blog-server/dist/server.js
+++ blog-server/dist/server.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log("Hello World!");
+Object.defineProperty(exports, "__esModule", { value: true });
+var apollo_server_1 = require("apollo-server");
+var type_defs_1 = require("./type-defs");
+var resolvers_1 = require("./resolvers");
+var server = new apollo_server_1.ApolloServer({ typeDefs: type_defs_1.typeDefs, resolvers: resolvers_1.resolvers });
+server.listen().then(function (_a) {
+    var url = _a.url;
+    console.log("\uD83D\uDE80  Server ready at " + url);
+});
